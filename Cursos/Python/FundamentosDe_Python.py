27c3d64882e4530f4b7ca2021f1ceaf73b19d69f
Nahuel <gnahuel014@gmail.com>
2022-04-30 00:49:37 -0300

    Funciones Integradas

--- Cursos/Python/FundamentosDe_Python.py
+++ Cursos/Python/FundamentosDe_Python.py
@@ -262,3 +262,153 @@ print("El resultado es: ",resultado)
 
 '''                                           ---------------------------------  Salida de Datos  ----------------------------------
 
+    Esto es la forma en que vamos a pasar todo el codigo para interacutar o ver en la consola.
+
+    PRINT():
+    La función Python print() toma cualquier número de parámetros y los imprime en una línea de texto. 
+    Cada uno de los elementos se convierte a formato de texto, separados por espacios, y hay un único '\n' al final (el carácter de "nueva línea"). 
+    Cuando se llama con cero parámetros, print() simplemente imprime el '\n' y nada más.
+
+    FORMAT:
+    La función format nos permite incluir en una cadena, texto ordinario y caracteres de formateo, que representan un tipo en particular de datos, tales como entero, 
+    cadena o flotante (Beazley, 2009).
+
+    PrintF:
+    La función printf (que deriva su nombre de “print formatted”) imprime un mensaje por pantalla utilizando una “cadena de formato” que incluye las instrucciones para mezclar 
+    múltiples cadenas en la cadena final a mostrar por pantalla.
+
+'''
+# ------------ PRINT -------------
+    nombre = 'Nahuel'
+    edad = 18
+
+    print("hola"nombre,"tienes",edad,"años") # esto es una salida de datos
+
+# ----------- FORMAT -------------
+    nombre = 'Nahuel'
+    edad = 18
+
+    print("hola {} tienes {} años".format(nombre,edad)) # esto es otra salida de datos
+
+# ----------- printF --------------
+    nombre = 'Nahuel'
+    edad = 18
+
+    print(f"hola {nombre} tienes {edad} años") # esta es una cadena de texto mas simple
+
+
+'''                                           ---------------------------------  Entrada de Datos  ----------------------------------
+    
+    Esto se usa usa para pedirle información al usuario, Ej: tenemos un programa para sumar, le tenemos que pedir al usuario que escriba un número para poder sumarlo, bueno ese
+    número que ponga el usuario va a hacer una entrada de datos.
+
+    Entrada de datos en Python:
+    Leer datos por teclado en Python se hace usando la función input(). 
+    Esta función generará una interrupción en el programa, esperando por una entrada. Dicha entrada, se entiende completada una vez el usuario presiona "enter" para confirmar 
+    los datos ingresados.
+'''
+
+# ------- Guardar datos de tipo Cadena | input ---------
+    nombre = input("Digite su nombre")
+
+    print(f"hola {nombre}")
+    
+# ------- Guardar datos de tipo Numericos Enteros | int --------
+    numero = int(input("introduzca un numero y se le sumara 1: "))
+
+    print(f"su número es {numero + 1}")
+
+# ------- Guardar datos de tipo Numericos Decimales | Float --------
+    numero = float(input("Escriba un número del 1 al 10: "))
+
+    print(f"El número que usted asigno es: {numero}")
+
+
+'''                                           ---------------------------------  Funciones Integradas  ----------------------------------
+
+    Sirven principalmente para hacer conversiones entre tipos de datos.
+    A la hora de programar en Python, es posible que te suenen palabras como file(), print(), open(), range(), etc. A esto se le llaman funciones integradas. 
+    Es decir, funciones que nos proporciona el propio lenguaje que se pueden ejecutar referenciándolas (llamadas).
+    
+    FUNCIONES INTEGRADAS:
+    int() Transforma una cadena a un entero (si no es posible da error)
+    float() Transforma una cadena a un flotante (si no es posible da error)
+    str() Transforma cualquier valor a una cadena
+    bin() Conversión de entero a binario
+    hex() Conversión de entero a hexadecimal
+    int(numero, base): Reconversión a entero
+    abs() Valor absoluto de un número (distancia)
+    round() Redondeo de un flotante a entero
+    eval() Evalúa una cadena como expresión, acepta variables si se han definido anteriormente
+    len() Longitud de una colección o cadena
+    help() Invoca el menú de ayuda del intérprete de Python
+'''
+# ----------- INT ------------
+    n = int("10")
+    print(n)
+
+    10 # Resultado
+
+# ----------- FLOAT ------------
+    f = float("10.5")
+    print(f)
+
+    10.5 # Resultado
+
+# ----------- STR ------------
+    c = "Un texto y dos números " + str(10) + " y " + str(3.14)
+    print(c)
+
+    Un texto y dos números 10 y 3.14 # Resultado
+
+# ----------- BIN ------------
+    bin(10)
+
+    '0b1010' # Resultado
+
+# ----------- HEX -----------
+    hex(10)
+
+    '0xa' # Resultado
+
+# ----------- INT(número,base) ------------
+    #Reconversión a entero (base 10):
+    print(int('0b1010', 2))
+    print(int('0xa', 16))
+
+    10 # Resultado
+    10 # Resultado
+
+# ----------- ABS ------------
+    abs(-10)
+
+    10 # Resultado
+
+# ----------- ROUND ------------
+    # Redondeo de un flotante a entero, menor de .5 a la baja, mayor o igual a .5 al alza:
+    print(round(5.5))
+    print(round(5.4))
+
+    6 # Resultado
+    5 # Resultado
+# ----------- Eval ------------
+    eval('2 + 5')
+    7 # Resultado
+
+    n = 10
+    eval('n * 10 - 5')
+
+    95 # Resultado
+
+# ----------- LEN ------------
+    print(len("Una cadena"))
+    print(len([]))
+
+    10 # Resultado
+    0 # Resultado
+
+
+
+
+
+
